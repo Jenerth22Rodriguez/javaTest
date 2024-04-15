7f8408279d5a34437116037bf1fe838b3043a9b5
Jenerth Rodriguez <Connerjamestill@gmail.com>
2024-04-15 16:16:52 -0400

    creating my first test using JUnit 4

---
+++ src/test/java/JenerthJavac/javaTestDemo/utilities/StringUtilTest.java
@@ -0,0 +1,10 @@
+package JenerthJavac.javaTestDemo.utilities;
+public class StringUtilTest {
+    public static void main(String[] args) {
+      String result =  StringUtil.repeat("Hello", 3);
+      System.out.println(result);
+
+        String result2 =  StringUtil.repeat("Hello", 1);
+        System.out.println(result2);
+    }
+}
