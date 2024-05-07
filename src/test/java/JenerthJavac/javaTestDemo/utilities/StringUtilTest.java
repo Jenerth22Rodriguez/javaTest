99a67ec632bb7eb0cff38e1af95ce5e5a6d229e6
Jenerth Rodriguez <Connerjamestill@gmail.com>
2024-05-07 19:45:48 -0400

    Organizing my Tests with JUnit

--- src/test/java/JenerthJavac/javaTestDemo/utilities/StringUtilTest.java
+++ src/test/java/JenerthJavac/javaTestDemo/utilities/StringUtilTest.java
@@ -4,10 +4,20 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class StringUtilTest {
-        @Test
-        public void testRepeat(){
+    @Test
 
-      Assert.assertEquals(StringUtil.repeat("Hello", 3), "HelloHelloHello");
-      Assert.assertEquals(StringUtil.repeat("Hello", 1), "Hello");
+    public void Repeat_string_once(){
+        Assert.assertEquals(StringUtil.repeat("Hello", 1), "Hello");
     }
+
+    @Test
+    public void Repeat_string_many(){
+        Assert.assertEquals(StringUtil.repeat("Hello", 3), "HelloHelloHello");
+    }
+
+    @Test
+    public void Repeat_string_zeroTime(){
+        Assert.assertEquals(StringUtil.repeat("Hello", 0), "");
+    }
+
 }
