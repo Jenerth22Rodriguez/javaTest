d3938e68ca6a0c59a2d6dd7f6a04192d682b59da
Jenerth Rodriguez <Connerjamestill@gmail.com>
2024-05-07 15:05:26 -0400

    adding my first two units teste

--- src/test/java/JenerthJavac/javaTestDemo/utilities/StringUtilTest.java
+++ src/test/java/JenerthJavac/javaTestDemo/utilities/StringUtilTest.java
@@ -1,10 +1,18 @@
 package JenerthJavac.javaTestDemo.utilities;
 public class StringUtilTest {
     public static void main(String[] args) {
-      String result =  StringUtil.repeat("Hello", 3);
-      System.out.println(result);
 
-        String result2 =  StringUtil.repeat("Hello", 1);
-        System.out.println(result2);
+      String result =  StringUtil.repeat("Hello", 3);
+
+      assertEquals(StringUtil.repeat("Hello", 3), "HelloHelloHello");
+
+      assertEquals(result, "Hello");
+    }
+
+    private static void assertEquals(String result, String helloHelloHello) {
+
+        if(!result.equals("HelloHelloHello")) {
+            throw new RuntimeException("Error encounter");
+        }
     }
 }
