9dc2a628f2b5f40d6728ee909b1ca8078d11be21
Jenerth Rodriguez <Connerjamestill@gmail.com>
2024-08-16 13:28:47 -0400

    check if the string is empty and writing their own tests

--- src/test/java/JenerthJavac/javaTestDemo/utilities/StringUtilTest.java
+++ src/test/java/JenerthJavac/javaTestDemo/utilities/StringUtilTest.java
@@ -3,21 +3,38 @@ package JenerthJavac.javaTestDemo.utilities;
 import org.junit.Assert;
 import org.junit.Test;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 public class StringUtilTest {
-    @Test
+//    @Test
+//
+//    public void Repeat_string_once(){
+//        Assert.assertEquals(StringUtil.repeat("Hello", 1), "Hello");
+//    }
+//
+//    @Test
+//    public void Repeat_string_many(){
+//        Assert.assertEquals(StringUtil.repeat("Hello", 3), "HelloHelloHello");
+//    }
+//
+//    @Test
+//    public void Repeat_string_zeroTime(){
+//        Assert.assertEquals(StringUtil.repeat("Hello", 0), "");
+//    }
 
-    public void Repeat_string_once(){
-        Assert.assertEquals(StringUtil.repeat("Hello", 1), "Hello");
+    @Test
+    public void string_is_not_empty() {
+        assertFalse(StringUtil.isEmpty("helloWorld"));
     }
 
     @Test
-    public void Repeat_string_many(){
-        Assert.assertEquals(StringUtil.repeat("Hello", 3), "HelloHelloHello");
+    public void string_is_empty() {
+        assertTrue(StringUtil.isEmpty(""));
     }
-
     @Test
-    public void Repeat_string_zeroTime(){
-        Assert.assertEquals(StringUtil.repeat("Hello", 0), "");
+    public void string_space_is_empty_string() {
+        assertTrue(StringUtil.isEmpty(" "));
     }
 
 }
